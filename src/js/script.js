1c496c88c1ee7d91bbfbcfa74846feeca98b216f
Eduardo Moraes <eduzinho2118@gmail.com>
2026-03-13 21:34:38 -0300

    primeiro commit

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+const listaLinks = [
+    { id: 1, url: "https://jovens-ibm-ipsep.netlify.app/" },
+    { id: 2, url: "https://github.com/EduardoMoraess" },
+    { id: 3, url: "https://www.instagram.com/eduardo_moraaees/" },
+    { id: 4, url: "" },
+]
+
+function abrirLink(idDesejado) {
+    // Encontra o objeto que tem o ID igual ao passado na função
+    const linkEncontrado = listaLinks.find(item => item.id === idDesejado);
+
+    if (linkEncontrado) {
+        window.open(linkEncontrado.url, "_blank");
+    } else {
+        console.error("Link não encontrado!");
+    }
+}
