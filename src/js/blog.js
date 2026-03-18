b164714718848b0839120b8a73e50ef90bd77bbb
Eduardo Moraes <eduzinho2118@gmail.com>
2026-03-18 18:12:16 -0300

    blog info

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,1 +1,16 @@
-console.log('ola mundo');
+const linksInfo = [
+    {id:1 , url: "https://youtu.be/c65yR7FWJsM?si=IzoVNvbwM0Ldj2V4"},
+    {id:2 , url: ""},
+    {id:3 , url: ""},
+]
+
+function acessarInfo(idInfo){
+    const infoEncontrada = linksInfo.find(item => item.id === idInfo);
+
+    if(infoEncontrada){
+        open(infoEncontrada.url, "_blank");
+    }
+    else{
+        console.error("Informação não emcontrada");
+    }
+}
